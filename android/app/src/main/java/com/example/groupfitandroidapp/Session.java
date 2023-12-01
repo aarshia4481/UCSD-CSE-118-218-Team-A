d33cee5c42304c8fc3e1f8a057e7d7ef1477bc48
Felix Linnemann <86649917+fellinn@users.noreply.github.com>
2023-11-30 16:38:30 -0800

    fix errors in Session.java

--- android/app/src/main/java/com/example/groupfitandroidapp/Session.java
+++ android/app/src/main/java/com/example/groupfitandroidapp/Session.java
@@ -2,10 +2,13 @@ package com.example.groupfitandroidapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -52,6 +55,6 @@ public class Session extends Activity {
 
                 // Start the new activity
                 startActivity(intent);
-        })
+        }});
     }
 }
