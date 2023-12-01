487d9cdaad762f527a34a5664143e86bacd79069
Felix Linnemann <86649917+fellinn@users.noreply.github.com>
2023-12-01 11:07:30 -0800

    add datamodel

---
+++ server/model/WorkoutDataModel.py
@@ -0,0 +1,37 @@
+
+
+
+# Define workout data model
+import uuid
+
+
+class WorkoutSession:
+    def __init__(self, session_name, creator_id, participants=[]):
+        self.session_name = session_name
+        self.creator_id = creator_id
+        self.participants = participants
+        self.session_id = str(uuid.uuid4())
+        self.active = True
+
+# class Participant:
+#     def __init__(self, name, age, gender, session_id):
+#         self.name = name
+#         self.age = age
+#         self.gender = gender
+#         self.session_id = session_id
+
+class HeartRateMeasurement:
+    def __init__(self, timestamp, heart_rate, participant_id, workout_session_id):
+        self.timestamp = timestamp
+        self.heart_rate = heart_rate
+        self.participant_id = participant_id
+        self.workout_session_id = workout_session_id
+
+class ExerciseLog:
+    def __init__(self, exercise_type, reps_completed, participant_id, workout_session_id):
+        self.exercise_type = exercise_type
+        self.reps_completed = reps_completed
+        self.participant_id = participant_id
+        self.workout_session_id = workout_session_id
+
+
